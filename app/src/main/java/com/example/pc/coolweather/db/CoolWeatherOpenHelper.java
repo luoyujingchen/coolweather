2ab4b977b5c9767d811d9627d54ba854db1901b6
PC <806969712@qq.com>
2015-08-16 01:05:53 +0800

    2015/8/16

---
+++ app/src/main/java/com/example/pc/coolweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,7 @@
+package com.example.pc.coolweather.db;
+
+/**
+ * Created by PC on 2015/8/15.
+ */
+public class CoolWeatherOpenHelper {
+}
---
+++ app/src/main/java/com/example/pc/coolweather/model/CoolWeatherDB.java
@@ -0,0 +1,7 @@
+package com.example.pc.coolweather.model;
+
+/**
+ * Created by PC on 2015/8/15.
+ */
+public class CoolWeatherDB {
+}
